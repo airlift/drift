ca957b08eb6cfd97b7d1fad4c2294232d470fbde
David Phillips <david@acz.org>
2017-07-14 12:21:07 -0700

    Use TType constants in ThriftProtocolType

--- drift-codec/src/main/java/io/airlift/drift/codec/ThriftProtocolType.java
+++ drift-codec/src/main/java/io/airlift/drift/codec/ThriftProtocolType.java
@@ -15,22 +15,24 @@
  */
 package io.airlift.drift.codec;
 
+import org.apache.thrift.protocol.TType;
+
 public enum ThriftProtocolType
 {
-    UNKNOWN((byte) 0),
-    BOOL((byte) 2),
-    BYTE((byte) 3),
-    DOUBLE((byte) 4),
-    I16((byte) 6),
-    I32((byte) 8),
-    I64((byte) 10),
-    STRING((byte) 11),
-    STRUCT((byte) 12),
-    MAP((byte) 13),
-    SET((byte) 14),
-    LIST((byte) 15),
-    ENUM((byte) 8), // same as I32 type
-    BINARY((byte) 11); // same as STRING type
+    UNKNOWN(TType.STOP),
+    BOOL(TType.BOOL),
+    BYTE(TType.BYTE),
+    DOUBLE(TType.DOUBLE),
+    I16(TType.I16),
+    I32(TType.I32),
+    I64(TType.I64),
+    STRING(TType.STRING),
+    STRUCT(TType.STRUCT),
+    MAP(TType.MAP),
+    SET(TType.SET),
+    LIST(TType.LIST),
+    ENUM(TType.I32),
+    BINARY(TType.STRING);
 
     private final byte type;
 
