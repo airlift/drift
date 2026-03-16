2dccdd61f2940badec0594589781d24d2b982818
David Phillips <david@acz.org>
2026-03-16 17:28:55 +0800

    Add trailing commas for annotation arrays

--- drift-codec/src/test/java/io/airlift/drift/codec/idlannotations/UnionWithIdlAnnotations.java
+++ drift-codec/src/test/java/io/airlift/drift/codec/idlannotations/UnionWithIdlAnnotations.java
@@ -23,7 +23,8 @@ import io.airlift.drift.annotations.ThriftUnionId;
 @ThriftUnion(
         idlAnnotations = {
                 @ThriftIdlAnnotation(key = "testkey1", value = "testvalue1"),
-                @ThriftIdlAnnotation(key = "testkey2", value = "testvalue2")})
+                @ThriftIdlAnnotation(key = "testkey2", value = "testvalue2"),
+        })
 public class UnionWithIdlAnnotations
 {
     private short unionType;
